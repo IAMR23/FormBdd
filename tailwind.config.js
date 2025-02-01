16b9609bdd1a96f27bfb77066399b391a24877ba
IASP23 <alexanderpilataxi232002@gmail.com>
2025-02-01 17:11:27 -0500

    Formulario

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}", // Asegúrate de incluir todos los tipos de archivos que usas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
